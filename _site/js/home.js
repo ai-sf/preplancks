17a72a163e60b061c56ff8136d233aca3672d2a7
Roberto Ciccarelli <robyciccarelli.rc@gmail.com>
2023-01-31 22:39:29 +0100

    Aggiorna

--- _site/js/home.js
+++ _site/js/home.js
@@ -1,5 +1,37 @@
-(function($){
-    $(function(){
+(function ($) {
+    $(function () {
+
+
+  // Set the date we're counting down to
+  var countDownDate = new Date( "Feb 5, 2023 23:59:59" ).getTime();
+
+  // Update the count down every 1 second
+  var x = setInterval(function() {
+
+    // Get today's date and time
+    var now = new Date().getTime();
+
+    // Find the distance between now and the count down date
+    var distance = countDownDate - now;
+
+    // Time calculations for days, hours, minutes and seconds
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    // Output the result in an element with id="countdown"
+    document.getElementById("countdown").innerHTML = "Le iscrizioni terminano tra " + days + "d " + hours + "h "
+    + minutes + "m " + seconds + "s ";
+
+    // If the count down is over, write some text
+    if (distance < 0) {
+      clearInterval(x);
+      document.getElementById("countdown").innerHTML = "ISCRIZIONI TERMINATE";
+    }
+  }, 1000);
+
+
         $("#index-page").css("padding-top", $(".slider").height()+50);
         $("#chevron-banner").click(function(){
             $('html, body').animate({
